93e0ecc625efd48c5b497ab88c05e98ec63a38df
Lorenzo <lorenzo.grespan@gmail.com>
2016-10-14 08:53:40 +0100

    getting somewhere

--- dns-queue.py
+++ dns-queue.py
@@ -25,22 +25,26 @@ class Prober(threading.Thread):
     def run(self):
         # this simulates how long the DNS query will take; substitute with the
         # actual DNS query command then save the output to a database
-        _will_take = random.random() * 10
+        _will_take = abs(random.gauss(0, 1) * 5)
         time.sleep(_will_take)
+
+
+def fill(d, amount):
+    for i in range(amount):
+        # add new probers, taking the hostnames from a list
+        t = Prober('another_host')
+        t.start()
+        d.append(t)
 
 
 def main():
     # this is the starting value - give it enough time to add all threads to
     # the queue
-    sleep_interval = random.random() * 10
+    sleep_time = random.random() * 10
 
     d = deque()
     ql = deque()
-    # initialize the queue
-    for i in range(MIN_DNS_SERVERS):
-        t = Prober('host1.example.com')
-        t.start()
-        d.append(t)
+    fill(d, MIN_DNS_SERVERS)
 
     ql.append(len(d))
 
@@ -61,18 +65,17 @@ def main():
             # print "Deque has length {}".format(len(d))
 
             # calculate how fast the queue has been growing
-            delta = len(d) - previous_len
-            rate = delta / sleep_interval
-            # print(rate)
+            delta = previous_len - len(d)
+            rate = delta / sleep_time
+            print('\tq: {}\tdelta: {}\trate: {}\t{}s'.format(len(d), delta, rate, sleep_time))
             ql.append(len(d))
 
-            if rate > 2:
-                # the queue is filling up too fast: increase sleep time it by 10%
-                sleep_interval += (sleep_interval * INCREASE_PERCENT)
+            if rate > 0:
+                sleep_time -= (sleep_time * INCREASE_PERCENT)
                 print('+', end="")
-            elif len(d) < MIN_DNS_SERVERS or rate < 0.5:
-                # the queue is emptying too fast: drastically reduce sleep time
-                sleep_interval /= HALVE_AMOUNT
+                fill(d, delta)
+            elif len(d) < MIN_DNS_SERVERS or rate < 0:
+                sleep_time -= (sleep_time * INCREASE_PERCENT)
                 print('-', end="")
             else:
                 # queue is neither empyting too fast nor filling up too fast -
@@ -80,12 +83,9 @@ def main():
                 print('.', end="")
                 # pass
 
-            # add new probers, taking the hostnames from a list
-            t = Prober('another_host')
-            t.start()
-            d.append(t)
+            previous_len = len(d)
 
-            time.sleep(sleep_interval)
+            time.sleep(sleep_time)
 
         except KeyboardInterrupt:
             print("\nPlease wait for all threads to finish...")
