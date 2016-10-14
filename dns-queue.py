7c6a99fabc8bf4604de525aa4b3b2fdf4c8ea448
Lorenzo <lorenzo.grespan@gmail.com>
2016-10-14 16:52:24 +0100

    Generates valid subdomain names, and saves output to file.

--- dns-queue.py
+++ dns-queue.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import argparse
 from collections import deque
 import itertools
+import os
 import random
 import string
 import sys
@@ -17,6 +18,17 @@ import threading
 INCREASE_PERCENT = 0.1
 MAX_DOMAIN_LEN = 3
 
+# valid domain names allow ASCII letters, digits and hyphen (and are case insensitive)
+# however see http://stackoverflow.com/questions/7111881/what-are-the-allowed-characters-in-a-sub-domain
+# and https://en.wikipedia.org/wiki/Domain_name#Internationalized_domain_names
+ALPHABET = ''.join([
+    string.lowercase,
+    string.digits,
+    # technically domains shouldn't start or end with a -
+    '-',
+    # add here unicode characters sets
+])
+
 
 res = deque()
 
@@ -25,14 +37,12 @@ class Prober(threading.Thread):
     def __init__(self, dns_server, target):
         # invoke Thread.__init__
         super(Prober, self).__init__()
-        # set the parameters needed for this thread e.g. hostname
-        print("Target: {}".format(target))
         self.target = target
         self.dns_server = dns_server
 
     def run(self):
         # this simulates how long the DNS query will take; substitute with the
-        # actual DNS query command then save the output to a database
+        # actual DNS query command
 
         ###
         # remove from here
@@ -40,41 +50,47 @@ class Prober(threading.Thread):
         # using a normal distribution to simulate real work
         _will_take = abs(random.gauss(0, 1) * 5)
         time.sleep(_will_take)
-        res.append("{} done in {}s".format(self.target, _will_take))
         #
         # to here
         ###
 
+        # then append the result to some form of storage
+        res.append("{} done in {}s".format(self.target, _will_take))
+
 
 def subdomain_gen():
-    """Generates all subdomains"""
+    """A generator that.. generates all subdomains from the given alphabet"""
     for i in range(MAX_DOMAIN_LEN):
-        for p in itertools.permutations(string.lowercase, i + 1):
+        for p in itertools.permutations(ALPHABET, i + 1):
             yield ''.join(p)
 
 
-sub = subdomain_gen()
-
-
-def fill(d, amount, dom):
+def fill(d, amount, dom, sub):
     for i in range(amount):
-        # add new probers, taking the hostnames from a list or a generator
+        # calls next() on the generator to get the next iteration (or next subdomain)
         t = Prober('dns_server', '{}.{}'.format(sub.next(), dom))
         t.start()
         d.append(t)
 
 
-def main(dom, max_running_threads):
-    print("-: increase queue ckeck interval by {}%\n.: no change\n".format(INCREASE_PERCENT))
+def main(dom, max_running_threads, outfile, overwrite):
+    if os.path.exists(outfile):
+        if overwrite is False:
+            raise SystemExit("Specified file {} exists and overwrite option (-f) not set".format(outfile))
+        else:
+            print("Overwriting output file {}".format(outfile))
+    print("-: queue ckeck interval increased by {}%\n.: no change\n".format(INCREASE_PERCENT))
+
     # this is the starting value - it will adjust it according to depletion rate
     sleep_time = 0.5
 
+    # the main queue containing all threads
     d = deque()
-    fill(d, max_running_threads, dom)
+    # the subdomain generator
+    sub = subdomain_gen()
 
-    # for plotting
-    # ql = deque()
-    # ql.append(len(d))
+    # fill the queue ip to max for now
+    fill(d, max_running_threads, dom, sub)
 
     previous_len = len(d)
     running = True
@@ -85,30 +101,22 @@ def main(dom, max_running_threads):
             for el in range(len(d)):
                 _t = d.popleft()
                 if _t.is_alive():
-                    # put it back and continue
+                    # put it back in the queue until next iteration
                     d.append(_t)
-                else:
-                    # thread was done - we remove it from the queue
-                    pass
+
             # calculate how fast the queue has been changind
             delta = previous_len - len(d)
             rate = delta / sleep_time
             # print('\tq: {}\tdelta: {}\trate: {}\t{}s'.format(len(d), delta, rate, sleep_time))
-            # ql.append(len(d))
 
             if rate > 0 and delta > max_running_threads / 10:
                 sleep_time -= (sleep_time * INCREASE_PERCENT)
                 print('+', end="")
             else:
                 sleep_time += (sleep_time * INCREASE_PERCENT)
-                # print('-', end="")
-                # else:
-                # queue is neither empyting too fast nor filling up too fast -
-                # we're OK
                 print('.', end="")
-                # pass
 
-            fill(d, delta, dom)
+            fill(d, delta, dom, sub)
             previous_len = len(d)
 
         except KeyboardInterrupt:
@@ -117,19 +125,21 @@ def main(dom, max_running_threads):
         finally:
             sys.stdout.flush()
 
+    # waiting for all threads to finish, popping them one by one and join() each...
     for el in range(len(d)):
         t = d.popleft()
         t.join()
-    print(res)
-    # use: gnuplot 'plot "data.txt" with lines' to see queue
-    # with open('data.txt', 'w') as f:
-    #     for i, el in enumerate(ql):
-    #         f.write('{} {}\n'.format(i, el))
+    with open(outfile, 'w') as f:
+        for r in res:
+            f.write('{}\n'.format(r))
+    print("Results written into file {}".format(outfile))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("domain")
     parser.add_argument("max_running_threads", type=int)
+    parser.add_argument("savefile", default="out.txt")
+    parser.add_argument("-f", "--force-overwrite", default=False, action='store_true')
     args = parser.parse_args()
-    main(args.domain, args.max_running_threads)
+    main(args.domain, args.max_running_threads, args.savefile, args.force_overwrite)
