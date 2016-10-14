a76926456e91aada4239f1b19d333dbdf8e1c7aa
Lorenzo <lorenzo.grespan@gmail.com>
2016-10-14 09:16:39 +0100

    Stays nicely below max (within 10%), not too aggressive

--- dns-queue.py
+++ dns-queue.py
@@ -13,46 +13,54 @@ import threading
 
 MIN_DNS_SERVERS = 40
 INCREASE_PERCENT = 0.1
-HALVE_AMOUNT = 3
 
 
 class Prober(threading.Thread):
     def __init__(self, dns_host):
         # invoke Thread.__init__
         super(Prober, self).__init__()
+        # set the parameters needed for this thread e.g. hostname
         self.dns_host = dns_host
 
     def run(self):
         # this simulates how long the DNS query will take; substitute with the
         # actual DNS query command then save the output to a database
+
+        ###
+        # remove from here
+        #
+        # using a normal distribution to simulate real work
         _will_take = abs(random.gauss(0, 1) * 5)
         time.sleep(_will_take)
+        #
+        # to here
+        ###
 
 
 def fill(d, amount):
     for i in range(amount):
-        # add new probers, taking the hostnames from a list
+        # add new probers, taking the hostnames from a list or a generator
         t = Prober('another_host')
         t.start()
         d.append(t)
 
 
 def main():
-    # this is the starting value - give it enough time to add all threads to
-    # the queue
-    sleep_time = random.random() * 10
+    # this is the starting value - it will adjust it according to depletion rate
+    sleep_time = 0.5
 
     d = deque()
-    ql = deque()
     fill(d, MIN_DNS_SERVERS)
 
+    # for plotting
+    ql = deque()
     ql.append(len(d))
 
-    # initial value
     previous_len = len(d)
     running = True
     while running:
         try:
+            time.sleep(sleep_time)
             # go through the queue and remove the threads that are done
             for el in range(len(d)):
                 _t = d.popleft()
@@ -62,30 +70,26 @@ def main():
                 else:
                     # thread was done - we remove it from the queue
                     pass
-            # print "Deque has length {}".format(len(d))
-
-            # calculate how fast the queue has been growing
+            # calculate how fast the queue has been changind
             delta = previous_len - len(d)
             rate = delta / sleep_time
             print('\tq: {}\tdelta: {}\trate: {}\t{}s'.format(len(d), delta, rate, sleep_time))
             ql.append(len(d))
 
-            if rate > 0:
+            if rate > 0 and delta > MIN_DNS_SERVERS / 10:
                 sleep_time -= (sleep_time * INCREASE_PERCENT)
                 print('+', end="")
-                fill(d, delta)
-            elif len(d) < MIN_DNS_SERVERS or rate < 0:
-                sleep_time -= (sleep_time * INCREASE_PERCENT)
-                print('-', end="")
             else:
+                sleep_time += (sleep_time * INCREASE_PERCENT)
+                # print('-', end="")
+                # else:
                 # queue is neither empyting too fast nor filling up too fast -
                 # we're OK
                 print('.', end="")
                 # pass
 
+            fill(d, delta)
             previous_len = len(d)
-
-            time.sleep(sleep_time)
 
         except KeyboardInterrupt:
             print("\nPlease wait for all threads to finish...")
@@ -99,5 +103,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print("+: increase sleep time by {}%\n-:decrease sleep time by {}\n.: no change\n".format(INCREASE_PERCENT, HALVE_AMOUNT))
+    print("+: increase sleep time by {}%\n-:decrease sleep time by {}\n.: no change\n".format(INCREASE_PERCENT, INCREASE_PERCENT))
     main()
